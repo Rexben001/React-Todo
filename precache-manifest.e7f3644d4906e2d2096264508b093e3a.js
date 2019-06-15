ecdc57eb855e73d650ddca5ef333c5c7a9cf6d4c
Rexben <rexben.rb@gmail.com>
2019-06-15 11:20:42 +0100

    Updates

---
+++ precache-manifest.e7f3644d4906e2d2096264508b093e3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464e42ec9e0927a6160a20f67b89d119",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "0237b506a6b5a84eb2e5",
+    "url": "/React-Todo/static/css/main.32742efc.chunk.css"
+  },
+  {
+    "revision": "6dbe5af2479c213cfeb3",
+    "url": "/React-Todo/static/js/2.6a1e20b3.chunk.js"
+  },
+  {
+    "revision": "0237b506a6b5a84eb2e5",
+    "url": "/React-Todo/static/js/main.c9772d75.chunk.js"
+  },
+  {
+    "revision": "8cccfb14f8793be292ed",
+    "url": "/React-Todo/static/js/runtime~main.58a39a8a.js"
+  },
+  {
+    "revision": "a3906765aa0cfb04d7dd1bdabd00ea3d",
+    "url": "/React-Todo/static/media/bg-image.a3906765.jpg"
+  }
+]);
